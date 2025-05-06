cec004a62a06aef85bb2dca687e4b06f866527a6
Sahil <sahilmahbub13@gmail.com>
2025-05-06 08:55:41 +0200

    Favicon Added

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TimeSync - Medical Appointment Management System</title>
+    <link rel="icon" href="TimeSync.png" type="image/x-icon">
     <style>
         :root {
             --primary: #2c6cff;
